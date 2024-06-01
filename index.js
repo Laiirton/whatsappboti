32011bf19a5320b0253b3e1f5fffa7380ae38516
Laiirton <130435095+Laiirton@users.noreply.github.com>
2024-06-01 19:27:36 -0300

    add gemini api functions

--- index.js
+++ index.js
@@ -5,6 +5,46 @@ const ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 ffmpeg.setFfmpegPath(ffmpegPath);
 
+// AI GOOGLE API
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = "AIzaSyCem06DOhmjJxz9qireL64r4Nt8L3lyVk0"
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function getGeminiResponse(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    safetySettings: [
+      { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_NONE },
+      { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_NONE },
+      { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_NONE },
+      { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_NONE },
+    ], 
+    history: [], 
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();
+}
+
+
+
 create({
   session: "whatsapp boti",
   catchQR: (base64Qr, asciiQR) => console.log(asciiQR),
@@ -16,9 +56,15 @@ create({
 
 async function start(client) {
   client.onMessage(async (message) => {
-    if (message) {
-      console.log(message);
-    }
+
+
+    if(message.body.startsWith('coiso'))
+      {
+        const prompt = message.body.replace('coiso', '').trim();
+        const response = await getGeminiResponse(prompt);
+        console.log(response)
+        await client.sendText(message.from, response);
+      }
 
     if (message.body === "!fig") {
       if (message.quotedMsg && message.quotedMsg.type === "image") {
@@ -153,3 +199,4 @@ async function start(client) {
     }
   });
 }
+
